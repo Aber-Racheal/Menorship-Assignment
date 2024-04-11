f3a0bb7cb20a95899765e7e0ab861df53b7f3451
Aber-Racheal <rachealaberr@gmail.com>
2024-04-11 14:01:31 +0300

    mentor

---
+++ mentor.py
@@ -0,0 +1,4 @@
+numbers=[1,3,5,8,5,1,6,6,2,2]
+distinctNums=list(set(numbers))
+
+print(distinctNums)
---
+++ mentorship.js
@@ -0,0 +1,9 @@
+lists=[1,1,2,3,4,5,5,6,7,8]
+
+    let nonDup=[...new Set(lists)];
+
+    console.log(nonDup)
+
+
+
+
